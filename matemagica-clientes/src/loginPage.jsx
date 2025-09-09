b5967bdcc1454106ae17d7b5269f76053b52fa1e
Pedro Henrique <pedrohenrique.marineli@gmail.com>
2025-09-09 15:42:55 -0300

    add logo

--- matemagica-clientes/src/loginPage.jsx
+++ matemagica-clientes/src/loginPage.jsx
@@ -1,17 +1,5 @@
 import React, { useState } from 'react';
 
-import loginImage from './assets/kids-img-login.jpg';
-
-// Componente para o logo "Matemágica"
-const LogoIcon = () => (
-  <svg className="h-8 w-auto text-purple-700" viewBox="0 0 200 50" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path d="M22.63,32.63c-2.4,1.8-5.33,2.83-8.8,2.83-8.8,0-13.8-5.4-13.8-12.9,0-7.5,5-12.9,13.8-12.9,3.47,0,6.4,1.03,8.8,2.83l-2.5,3.8c-1.8-1.2-3.9-2-6.3-2-5.4,0-8.8,3.9-8.8,9.1s3.4,9.1,8.8,9.1c2.4,0,4.5-0.8,6.3-2Z"/>
-    <text x="35" y="35" fontFamily="Arial, sans-serif" fontSize="30" fontWeight="bold" fill="#581c87">Matemágica</text>
-    <path fill="#a855f7" d="M30.3,5.8c-1.8-1.8-4.2-2.8-6.8-2.8-5.2,0-9.4,4.2-9.4,9.4s4.2,9.4,9.4,9.4c2.6,0,5-1,6.8-2.8l-2.5-2.5c-1.2,1.2-2.8,1.8-4.3,1.8-3.3,0-6-2.7-6-6s2.7-6,6-6c1.5,0,3.1,0.6,4.3,1.8Z" transform="translate(-10, -2) scale(0.8)"/>
-  </svg>
-);
-
-
 // Componente principal da Página de Login
 export default function LoginPage() {
   const [emailOrUsername, setEmailOrUsername] = useState('');
@@ -26,11 +14,6 @@ export default function LoginPage() {
     }
     setError('');
     console.log('Tentativa de login com:', { login: emailOrUsername, password });
-    
-    // TODO: Implementar a chamada à API aqui
-    // Ex: axios.post('/api/gateway/auth/login', { login: emailOrUsername, password })
-    //     .then(response => { /* salvar token e redirecionar */ })
-    //     .catch(err => { setError('Email/usuário ou senha inválidos.'); });
   };
 
   return (
@@ -39,7 +22,7 @@ export default function LoginPage() {
       {/* Coluna da Imagem (visível em telas grandes) */}
       <div className="hidden lg:block relative">
         <img
-          src="./assets/kids-img-login.jpg"
+          src="/kids-img-login.jpg"
           alt="Crianças estudando felizes"
           className="absolute inset-0 h-full w-full object-cover"
         />
@@ -51,7 +34,7 @@ export default function LoginPage() {
           
           {/* Cabeçalho com Logo */}
           <div className="flex flex-col items-center text-center">
-            <LogoIcon />
+            <img src="/logo.svg" alt="logo"className='absolute top-0 right-0 w-3xs'/>
             <h1 className="text-4xl font-bold text-gray-800 mt-6">
               Bem vindo(a)!
             </h1>
