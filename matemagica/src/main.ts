d1b8779af4c3a14655e1d08c57773e7f5a5c46c5
Fabio Andrade <fabioandrade697@gmail.com>
2025-11-08 23:18:55 -0300

    changing main.ts for redirect to login when token is over

--- matemagica/src/main.ts
+++ matemagica/src/main.ts
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { useUserStore } from './userStore'
 import axios from 'axios'
+import { showNotification } from './stores/notificationStore'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -19,5 +20,30 @@ const userStore = useUserStore()
 if (userStore.data?.token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${userStore.data.token}`
 }
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000',
+})
+
+api.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    (error) => {
+        const userStore = useUserStore()
+        
+        const status = error.response ? error.response.status : null
+
+        if (status === 401) {
+            showNotification('Token expirado ou não autorizado. Redirecionando para login.', 'bg-red-500')
+
+            userStore.clearUserData()
+
+            router.push('/login')
+        }
+
+        return Promise.reject(error);
+    }
+);
 
 app.mount('#app')
